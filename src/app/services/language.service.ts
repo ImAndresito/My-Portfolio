ffc8f1de7579274a0c1450f53fea4020a47b09db
Andres <andres.villasanchez01@gmail.com>
2025-06-05 15:34:31 +0200

    feat(language): add multilingual support with http loading

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LanguageService {
+  private currentLanguage: string = 'en';
+  private translations: any = {};
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  loadLanguage(language: string) {
+    this.http
+      .get(`assets/i18n/${language}.json`)
+      .subscribe((data) => this.translations);
+    this.currentLanguage = language;
+  }
+
+  getTranslation(key: string): string {
+    return this.translations[key] || key;
+  }
+
+  getCurrentLanguage(): string {
+    return this.currentLanguage;
+  }
 }
