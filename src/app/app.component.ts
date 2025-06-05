a76799b8ab6da740380c6add8e57baf7778b52e9
Andres <andres.villasanchez01@gmail.com>
2025-06-05 11:44:57 +0200

    Creazione indroduction component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IntroductionComponent } from './components/introduction/introduction.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IntroductionComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'My-Portfolio';
