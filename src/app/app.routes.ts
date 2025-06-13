485247e4c6917e35513c1bb295e61d254565508e
Andres <andres.villasanchez01@gmail.com>
2025-06-13 13:10:07 +0200

    feat(projects): add projects component with routing
    Add new projects component with basic template and routing from what-ido component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { WhatIDoComponent } from './components/what-ido/what-ido.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: WhatIDoComponent,
     title: '',
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+    title: 'Projects',
   },
 ];
--- src/app/components/what-ido/what-ido.component.ts
+++ src/app/components/what-ido/what-ido.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '../../pipes/translate.pipe';
 import { LanguageService } from '../../services/language.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-what-ido',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, RouterLink],
   templateUrl: './what-ido.component.html',
   styleUrl: './what-ido.component.css',
 })
