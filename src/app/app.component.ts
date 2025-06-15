e045bf786199b41ed82732f868941d7391479f8a
Andres <andres.villasanchez01@gmail.com>
2025-06-15 18:08:10 +0200

    feat(buttons): add new buttons component to app
    Add new buttons component with basic template and test file. Include the component in app component imports and template.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IntroductionComponent } from './components/introduction/introduction.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, IntroductionComponent, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    IntroductionComponent,
+    HeaderComponent,
+    ButtonsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
